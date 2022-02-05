1c0cc18d763bbe852dff96ffd84f5a3c6c9f8db1
Saad Mehmood <innovationstechn@gmail.com>
2022-02-05 16:41:58 +0500

    Made seperate UIs for landscape and potrait view on login page.

--- lib/pages/login/login_view.dart
+++ lib/pages/login/login_view.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,126 +29,222 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: buildFooter(),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: OrientationBuilder(
+              builder: (BuildContext context, Orientation orientation) {
+                return MediaQuery.of(context).orientation == Orientation.portrait
+                    ? _portraitLayout()
+                    : _landscapeLayout();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _portraitLayout() {
     return ViewModelBuilder<LoginVM>.reactive(
-        viewModelBuilder: () => LoginVM(),
-        onModelReady: (loginVM) async {
-          await loginVM.initialize();
+      viewModelBuilder: () => LoginVM(),
+      onModelReady: (loginVM) async {
+        await loginVM.initialize();
 
-          emailController.text = loginVM.credentials?.name ?? "";
-          passwordController.text = loginVM.credentials?.password ?? "";
-        },
-        builder: (context, loginVM, _) {
-          final deviceHeight = SizerUtil.orientation == Orientation.landscape
-              ? SizerUtil.height / 100
-              : SizerUtil.width / 100;
-
-          return Scaffold(
-            body: SafeArea(
-              child: CustomScrollView(
-                slivers: [
-                  SliverToBoxAdapter(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (loginVM.ipAddress != null)
-                          CachedNetworkImage(
-                            fit: BoxFit.fitHeight,
-                            height: deviceHeight.round() * 51,
-                            imageUrl: loginVM.ipAddress ?? "",
-                            placeholder: (context, url) => const Center(
-                                child: CircularProgressIndicator()),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Form(
-                          key: _loginFormKey,
-                          child: SizedBox(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  TextFormField(
-                                    focusNode: emailFN,
-                                    controller: emailController,
-                                    onFieldSubmitted: (_) =>
-                                        FocusScope.of(context)
-                                            .requestFocus(passFN),
-                                    validator: (text) {
+        emailController.text = loginVM.credentials?.name ?? "";
+        passwordController.text = loginVM.credentials?.password ?? "";
+      },
+      builder: (context, loginVM, _) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (loginVM.ipAddress != null)
+              CachedNetworkImage(
+                fit: BoxFit.fitHeight,
+                imageUrl: loginVM.ipAddress ?? "",
+                placeholder: (context, url) =>
+                    const Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            Form(
+              key: _loginFormKey,
+              child: SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextFormField(
+                        focusNode: emailFN,
+                        controller: emailController,
+                        onFieldSubmitted: (_) =>
+                            FocusScope.of(context).requestFocus(passFN),
+                        validator: (text) {
 /*
-                                        if (!EmailValidator.validate(text!)) {
-                                          FocusScope.of(context)
-                                              .requestFocus(emailFN);
-                                          return "Please enter a valid email";
-                                        }
+                                          if (!EmailValidator.validate(text!)) {
+                                            FocusScope.of(context)
+                                                .requestFocus(emailFN);
+                                            return "Please enter a valid email";
+                                          }
 */
-                                    },
-                                    decoration: InputDecoration(
-                                      hintText: "Enter your email",
-                                      contentPadding: const EdgeInsets.fromLTRB(
-                                          12, 0, 20, 0),
-                                      errorStyle: const TextStyle(
-                                          color: Colors.redAccent,
-                                          fontSize: 12),
-                                      border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  TextFormField(
-                                    focusNode: passFN,
-                                    obscureText: true,
-                                    controller: passwordController,
-                                    onFieldSubmitted: (_) =>
-                                        FocusScope.of(context)
-                                            .requestFocus(loginFN),
-                                    validator: (text) {
-                                      if (text!.isEmpty) {
-                                        FocusScope.of(context)
-                                            .requestFocus(passFN);
-                                        return "Password is empty";
-                                      }
-                                    },
-                                    decoration: InputDecoration(
-                                      hintText: "Enter your password",
-                                      contentPadding: const EdgeInsets.fromLTRB(
-                                          12, 0, 20, 0),
-                                      errorStyle: const TextStyle(
-                                          color: Colors.redAccent,
-                                          fontSize: 12),
-                                      border: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(5.0),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 5),
-                                  buildLoginButton(context, loginVM),
-                                ],
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter your email",
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(12, 0, 20, 0),
+                          errorStyle: const TextStyle(
+                              color: Colors.redAccent, fontSize: 12),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        focusNode: passFN,
+                        obscureText: true,
+                        controller: passwordController,
+                        onFieldSubmitted: (_) =>
+                            FocusScope.of(context).requestFocus(loginFN),
+                        validator: (text) {
+                          if (text!.isEmpty) {
+                            FocusScope.of(context).requestFocus(passFN);
+                            return "Password is empty";
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter your password",
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(12, 0, 20, 0),
+                          errorStyle: const TextStyle(
+                              color: Colors.redAccent, fontSize: 12),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      buildLoginButton(context, loginVM),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _landscapeLayout() {
+    return ViewModelBuilder<LoginVM>.reactive(
+      viewModelBuilder: () => LoginVM(),
+      onModelReady: (loginVM) async {
+        await loginVM.initialize();
+
+        emailController.text = loginVM.credentials?.name ?? "";
+        passwordController.text = loginVM.credentials?.password ?? "";
+      },
+      builder: (context, loginVM, _) {
+        return Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: loginVM.ipAddress != null
+                    ? CachedNetworkImage(
+                        fit: BoxFit.fitHeight,
+                        imageUrl: loginVM.ipAddress ?? "",
+                        placeholder: (context, url) => const Center(
+                          child: CircularProgressIndicator(
+                            color: primaryColor,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      )
+                    : const Center(
+                        child: CircularProgressIndicator(color: primaryColor),
+                      ),
+              ),
+              Expanded(
+                child: Form(
+                  key: _loginFormKey,
+                  child: SizedBox(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextFormField(
+                            focusNode: emailFN,
+                            controller: emailController,
+                            onFieldSubmitted: (_) =>
+                                FocusScope.of(context).requestFocus(passFN),
+                            validator: (text) {
+/*
+                                              if (!EmailValidator.validate(text!)) {
+                                                FocusScope.of(context)
+                                                    .requestFocus(emailFN);
+                                                return "Please enter a valid email";
+                                              }
+*/
+                            },
+                            decoration: InputDecoration(
+                              hintText: "Enter your email",
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(12, 0, 20, 0),
+                              errorStyle: const TextStyle(
+                                  color: Colors.redAccent, fontSize: 12),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            focusNode: passFN,
+                            obscureText: true,
+                            controller: passwordController,
+                            onFieldSubmitted: (_) =>
+                                FocusScope.of(context).requestFocus(loginFN),
+                            validator: (text) {
+                              if (text!.isEmpty) {
+                                FocusScope.of(context).requestFocus(passFN);
+                                return "Password is empty";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              hintText: "Enter your password",
+                              contentPadding:
+                                  const EdgeInsets.fromLTRB(12, 0, 20, 0),
+                              errorStyle: const TextStyle(
+                                  color: Colors.redAccent, fontSize: 12),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          buildLoginButton(context, loginVM),
+                        ],
+                      ),
                     ),
                   ),
-                  SliverFillRemaining(
-                    hasScrollBody: false,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: buildFooter(),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ),
-          );
-        });
+            ],
+          ),
+        );
+      },
+    );
   }
 
   Widget buildLoginButton(BuildContext context, LoginVM loginVM) {
@@ -204,6 +303,7 @@ class _LoginViewState extends State<LoginView> {
         margin: const EdgeInsets.only(top: 20),
         padding: const EdgeInsets.all(8),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: const [
